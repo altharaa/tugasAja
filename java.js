498bf77338bedb71ddc44e7357f02609a41e981a
Alethea Maureen <althamrn@Bonis-MacBook-Pro.local>
2023-01-13 11:35:27 +0700

    first commit

---
+++ java.js
@@ -0,0 +1,75 @@
+// let continueBtn = document . getElementById("continueBtn");
+// let googleBtn = document. getElementById("googleBtn");
+// let inputNumber = document. getElementById("inputNumber");
+// let title = document. getElementById("title");
+
+// continueBtn.onclick = function(){
+// nameField.style.maxHeight = "0";
+// title.innerhTML = "Sign In";
+// continueBtn.classList.add("disable");
+// googleßtn.classList.remove("disable");
+// }
+
+// $(document).ready(function() {
+
+//     function toggleSidebar() {
+//       $(".button").toggleClass("active");
+//       $("main").toggleClass("move-to-left");
+//       $(".sidebar-item").toggleClass("active");
+//     }
+  
+//     $(".button").on("click tap", function() {
+//       toggleSidebar();
+//     });
+  
+//     $(document).keyup(function(e) {
+//       if (e.keyCode === 27) {
+//         toggleSidebar();
+//       }
+//     });
+  
+//   });
+
+// let nav = false;
+
+// const handleClick = () => {
+//     const menuLinks = document.querySelector(".menuLinks");
+
+//         if(nav) {
+//         menuLinks.style.cssText = "right:70%; transition:all 0.3s";
+//         nav = !nav;
+//         } else {
+//         menuLinks.style.cssText = "right:0%; transition:all 0.3s";
+//         nav = !nav;
+//         }
+// };
+
+// $(document).ready(function() {
+
+//     function toggleSidebar() {
+//       $(".button").toggleClass("active");
+//       $("main").toggleClass("move-to-left");
+//       $(".sidebar-item").toggleClass("active");
+//     }
+  
+//     $(".button").on("click tap", function() {
+//       toggleSidebar();
+//     });
+  
+//     $(document).keyup(function(e) {
+//       if (e.keyCode === 27) {
+//         toggleSidebar();
+//       }
+//     });
+  
+//   });
+
+
+addEventListener("click", function() {
+let hamburger = document.getElementsByClassName(".custom-bar")
+let condition =false
+  if (condition) {  
+    console.log("true");
+  
+  }
+})
